e4ddbf6c8e7e01ea95a44f5042060fe8b10d3006
Bilal Bhatti <bbhatti@gmail.com>
2021-11-29 19:54:59 -0600

    go curly

---
+++ cmd/curly/main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"os"
+
+	"github.com/nojima/httpie-go"
+	"github.com/nojima/httpie-go/exchange"
+	"github.com/nojima/httpie-go/flags"
+	"github.com/nojima/httpie-go/input"
+	"github.com/nojima/httpie-go/output"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	if err := httpie.Main(); err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func Main() error {
+	// Parse flags
+	args, usage, optionSet, err := flags.Parse(os.Args)
+	if err != nil {
+		return err
+	}
+	inputOptions := optionSet.InputOptions
+	exchangeOptions := optionSet.ExchangeOptions
+	outputOptions := optionSet.OutputOptions
+
+	// Parse positional arguments
+	in, err := input.ParseArgs(args, os.Stdin, &inputOptions)
+	if _, ok := errors.Cause(err).(*input.UsageError); ok {
+		usage.PrintUsage(os.Stderr)
+		return err
+	}
+	if err != nil {
+		return err
+	}
+
+	// Send request and receive response
+	status, err := Exchange(in, &exchangeOptions, &outputOptions)
+	if err != nil {
+		return err
+	}
+
+	if exchangeOptions.CheckStatus {
+		os.Exit(getExitStatus(status))
+	}
+
+	return nil
+}
+
+func getExitStatus(statusCode int) int {
+	if 300 <= statusCode && statusCode < 600 {
+		return statusCode / 100
+	}
+	return 0
+}
+
+func Exchange(in *input.Input, exchangeOptions *exchange.Options, outputOptions *output.Options) (int, error) {
+	// Prepare printer
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	printer := output.NewPrinter(writer, outputOptions)
+
+	// Build HTTP request
+	request, err := exchange.BuildHTTPRequest(in, exchangeOptions)
+	if err != nil {
+		return -1, err
+	}
+
+	// Print HTTP request
+	if outputOptions.PrintRequestHeader || outputOptions.PrintRequestBody {
+		// `request` does not contain HTTP headers that HttpClient.Do adds.
+		// We can get these headers by DumpRequestOut and ReadRequest.
+		dump, err := httputil.DumpRequestOut(request, true)
+		if err != nil {
+			return -1, err // should not happen
+		}
+		r, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(dump)))
+		if err != nil {
+			return -1, err // should not happen
+		}
+		defer r.Body.Close()
+
+		// ReadRequest deletes Host header. We must restore it.
+		if request.Host != "" {
+			r.Header.Set("Host", request.Host)
+		} else {
+			r.Header.Set("Host", request.URL.Host)
+		}
+
+		if outputOptions.PrintRequestHeader {
+			if err := printer.PrintRequestLine(r); err != nil {
+				return -1, err
+			}
+			if err := printer.PrintHeader(r.Header); err != nil {
+				return -1, err
+			}
+		}
+		if outputOptions.PrintRequestBody {
+			if err := printer.PrintBody(r.Body, r.Header.Get("Content-Type")); err != nil {
+				return -1, err
+			}
+		}
+		fmt.Fprintln(writer)
+		writer.Flush()
+	}
+
+	// Send HTTP request and receive HTTP request
+	httpClient, err := exchange.BuildHTTPClient(exchangeOptions)
+	if err != nil {
+		return -1, err
+	}
+	resp, err := httpClient.Do(request)
+	if err != nil {
+		return -1, errors.Wrap(err, "sending HTTP request")
+	}
+	defer resp.Body.Close()
+
+	if outputOptions.PrintResponseHeader {
+		if err := printer.PrintStatusLine(resp.Proto, resp.Status, resp.StatusCode); err != nil {
+			return -1, err
+		}
+		if err := printer.PrintHeader(resp.Header); err != nil {
+			return -1, err
+		}
+		writer.Flush()
+	}
+
+	if outputOptions.Download {
+		file := output.NewFileWriter(in.URL, outputOptions)
+
+		if err := printer.PrintDownload(resp.ContentLength, file.Filename()); err != nil {
+			return -1, err
+		}
+		writer.Flush()
+
+		if err = file.Download(resp); err != nil {
+			return -1, err
+		}
+	} else {
+		if outputOptions.PrintResponseBody {
+			if err := printer.PrintBody(resp.Body, resp.Header.Get("Content-Type")); err != nil {
+				return -1, err
+			}
+		}
+	}
+
+	return resp.StatusCode, nil
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/bilal-bhatti/curly
+
+go 1.17
+
+require (
+	code.cloudfoundry.org/bytefmt v0.0.0-20200131002437-cf55d5288a48 // indirect
+	github.com/VividCortex/ewma v1.1.1 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mtibben/androiddnsfix v0.0.0-20200907095054-ff0280446354 // indirect
+	github.com/nojima/httpie-go v0.7.0 // indirect
+	github.com/pborman/getopt v0.0.0-20190409184431-ee0cd42419d3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/vbauerster/mpb/v5 v5.0.2 // indirect
+	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
+)
